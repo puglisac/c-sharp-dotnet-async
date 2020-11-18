08bb82bc24c826a15347eda0dd103af8a5c0b971
puglisac <alan.c.puglisi@gmail.com>
2020-11-18 10:51:26 -0600

    finished section

---
+++ LearningAsync/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LearningAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Downloading file");
+            Download();
+            Console.WriteLine("After file download is called");
+        }
+        static void Download()
+        {
+            Network.Download(() => Console.WriteLine("Download complete"));           
+        }
+        //imaginary external network library
+        class Network
+        {
+            static public void Download(Action callback)
+            {
+                Task.Run(() =>
+                {
+                    Thread.Sleep(3000);
+                    callback();
+                });
+            }
+        }
+    }
+}
---
+++ LearningAsyncAwait/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LearningAsyncAwayStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("downloading file");
+            Download();
+            Console.ReadLine();
+
+        }
+
+        static async void Download()
+        {
+            HttpClient client = new HttpClient();
+            var data = await client.GetStringAsync("http://rouxacademy.com");
+            Console.WriteLine("Download complete" + data);
+        }
+    }
+}
